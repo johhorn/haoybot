4763d580a7310fdcc0be8ce6f4448c782e2618d0
Johanne Håøy Horn <johanne.hh@gmail.com>
2017-12-19 21:37:50 +0100

    Prøver å få det til å fungere

--- scripts/kaffe.js
+++ scripts/kaffe.js
@@ -1,11 +1,16 @@
-'use strict';
+/*
+* Tell ned til kaffen er klar på OPF! Estimert tidsbruk er 6 minutter
+*/
 
 module.exports = robot => {
+  'use strict';
+
+  robot.commands.push('kaffen er satt på - begynn nedtelling til kaffen er klar');
   robot.hear(
     /kaffen er satt på/i,
     response => setTimeout(
       () => response.send('Kaffen er klar!'),
-      6*1000*60,
+      6
     )
   );
 }
